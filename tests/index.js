739b6a84a1eb1467e4e184a4fa3671fa3e7eebe0
HypnoticSiege <luis@hypnoticsiege.net>
2022-10-21 08:44:14 -0400

    bug(tests) make lib work with js

---
+++ tests/index.js
@@ -0,0 +1,8 @@
+const MTAClient = require('../lib/client/client').default;
+const Client = new MTAClient("YOUR_TOKEN");
+
+async function getStop(id) {
+    console.log(await Client.getStop(id))
+};
+
+getStop(202650);
